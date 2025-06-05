89a40d2832addc1531e3452c159c6f31edac15ea
taya0v0 <taya0v0@gmail.com>
2025-06-05 05:13:01 +0300

    Fix Render deployment

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -8,9 +8,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1234567890abcdefghijklmnopqrstuvwxyz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+
+
+
+ALLOWED_HOSTS = [    'minor-portfolio-app.onrender.com',  # Ваш домен на Render
+    'localhost',                         # Для локальной разработки
+    '127.0.0.1', ]
 
 # Application definition
 INSTALLED_APPS = [
